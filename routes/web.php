6624599c251f5d5d0867da3243ffc81110d07130
JaimeM28 <mirandaserranojaimemanuelnuel@gmail.com>
2023-02-16 11:08:35 -0600

    rutas y controladores

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+//se llama al controlador 
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+    // return view('welcome');
+    return "Hola crayola";
+});
+
+Route::get('cursos', function(){
+    return "Bienvenido a la pagina curso";
+});
+
+//ruta estatica, debe ir antes de una con variable 
+Route::get('cursos/create', function(){
+    return "Crear curso ";
+});
+
+
+//Creación de ruta con variable, se pone entre corchetes en url
+//y luego la función anonima recibe la variable 
+Route::get('cursos/{curso}', function($curso){
+    return "bienvenido al $curso";
+});
+
+// //ruta con dos variables 
+// Route::get('cursos/{curso}/{categoria}', function ($curso, $categoria) {
+//     return "bienvenido al $curso de la categoria $categoria";
+// });
+
+//Si se añade signo de interrogación, será opcional. Se debe inicializar la variable opcional
+//en null 
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria=null) {
+    if($categoria){
+        return "bienvenido al $curso de la categoria $categoria";
+    }else {
+        return "Bienvenido al curso: $curso";
+    }
+   
+});
+
+
+
+
