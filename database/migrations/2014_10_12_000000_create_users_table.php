d9b9e7eb5de588a480ef1be1f68f7e385a714af4
JaimeM28 <mirandaserranojaimemanuelnuel@gmail.com>
2023-02-19 21:52:55 -0600

    creando migración

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,23 +7,34 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations. (crear tabla)
      */
     public function up(): void
     {
+
+        /* 
+        Se usa el método create de la clase schema. El primer argumento es el 
+        nombre de la tabla y el segundo una función anonima, que recibe
+        como parametro un objeto Blueprint, que se usa para crear columnas de la tabla
+
+        */ 
+
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id(); //numero entero incremental
+            $table->string('name'); //varchar de nombre name, segundo parametro tamaño
+            //para mayor cantidad, usar el método text();
+
+            $table->string('email')->unique(); //varchar unico
+            $table->timestamp('email_verified_at')->nullable(); //fechas y nullable opcional
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken(); //token de sesión
+            $table->timestamps(); //2 columnas  created_at (nuevo registro  y update_at (actualización registro)
         });
     }
 
     /**
      * Reverse the migrations.
+     * Borrar tabla
      */
     public function down(): void
     {
