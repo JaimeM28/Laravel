e6d2ee0c41dbea6ebef5327fa83ab5195463ab20
JaimeM28 <mirandaserranojaimemanuelnuel@gmail.com>
2023-02-22 10:48:58 -0600

    Cambiando a postgres y añadiendo formulario

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -4,5 +4,27 @@
 
 @section('content') 
     <h1> Crear cursos </h1>
+    <form action=""> 
+        <label >
+            Nombre: <br>
+
+            <input type="text" name="name"> 
+        </label>
+        <br>
+
+        <label>
+            Descripcion:<br>
+
+            <textarea name="descripcion" rows="5">
+            </textarea>
+        </label><br>
+
+        <label>
+            Categoria: <br>
+
+            <input type="text" name="categotia">
+        </label>
+        
+    </form>
 @endsection  
 
